4db93a321c64de75ec706ce633dbf061e0fab9cf
RanjeetR <ranjeet.hange@outlook.com>
2015-10-11 20:15:42 +0530

    style update

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var bw = require("browser-window")
 //var packager = require('electron-packager');
 app.on("ready", function () {
      mainWindow = new bw({
-        width:60,
-        height:400,
+        width:640,
+        height:340,
         resizable:false
     })
     mainWindow.loadUrl("file://"+__dirname+"/index.html");
