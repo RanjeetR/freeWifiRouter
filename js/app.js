33542ecdcb7318f26462daaa3f2431d2f913f1b6
RanjeetR <ranjeet.hange@outlook.com>
2015-10-11 23:32:21 +0530

    node code added

--- js/app.js
+++ js/app.js
@@ -13,6 +13,11 @@
     var networkname = document.getElementById("networkname");
     var password = document.getElementById("password");
 
+    //register exec
+    var exec = require('child_process').exec;
+    //stoping hotspot if already created
+
+
 
   //Register Events
     start.addEventListener('click',startHotspot);
@@ -24,8 +29,44 @@
     //Funtions
 
      function startHotspot (){
-         alert("comp");
-    }
+         setTimeout(function(){
+             exec('netsh wlan stop hostednetwork', function(error, stdout, stderr) {
+
+
+                 console.log('stdout: ' + stdout);
+                 console.log('stderr: ' + stderr);
+                 console.log("network stopeed");
+                 if (error !== null) {
+                     console.log('exec error: ' + error);
+                 }
+             });
+        },1600);
+
+        console.log(networkname.value  ,password.value);
+
+         exec('netsh wlan set hostednetwork mode=allow ssid= '+networkname.value+'  key='+password.value+' ', function(error, stdout, stderr) {
+             //console.log('stdout: ' + stdout);
+             //console.log('stderr: ' + stderr);
+             console.log("network starting");
+             console.log('netsh wlan set hostednetwork mode=allow ssid= '+networkname.value+'  key='+password.value+' ');
+             console.log(networkname.value);
+             if (error !== null) {
+                 console.log('exec error: ' + error);
+             }
+         });
+
+
+         setTimeout(function(){
+             exec('netsh wlan start hostednetwork ', function(error, stdout, stderr) {
+                 console.log('stdout: ' + stdout);
+                 console.log('stdout: ' + stderr);
+                 console.log("network started");
+                 if (error !== null) {
+                     console.log('exec error: ' + error);
+                 }
+             });
+         },3000)
+     }
      function ShowConnected (){
          alert("comp");
     }
